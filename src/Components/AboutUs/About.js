f7a5ec807b76e2634909674efc54b8657eeb44a3
unknown <akshayhiwase96@gmail.com>
2020-02-19 13:53:35 +0530

    first commit

---
+++ src/Components/AboutUs/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './About.module.css';
+
+const About = () => {
+    return (
+        <div className={classes.about_container}>
+            <h1>This Is AboutUs Page.... </h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/Components/Contacts/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from 'Contact.module.css';
+
+const contact = () => {
+    return (
+        <div className={classes.contact_container}>
+            <h1>This Is Contacts Page</h1>
+        </div>
+    )
+}
+
+export default contact;
